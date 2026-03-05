10661b5f63fa74ad03dae1dcfd1c2382aa041d15
Nicholas Hamilton-James <nhamiltonjames@apissys.com>
2026-03-05 12:14:43 +0100

    refactor: place all plugins in separate lua files & keep init.lua clean

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.opt.colorcolumn = '80'
+vim.cmd 'highlight ColorColumn ctermbg=0 guibg=darkred'
+
 return {}
--- lua/custom/plugins/linter.lua
+++ lua/custom/plugins/linter.lua
@@ -1,20 +1,26 @@
 -- Goto use :
 -- ]d next
 -- [d previous
-vim.opt.colorcolumn = '80'
-vim.cmd 'highlight ColorColumn ctermbg=0 guibg=darkred'
+
 return {
   {
     'mfussenegger/nvim-lint',
     run = 'pip3 install cpplint --upgrade',
+
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
         cpp = { 'cpplint' },
+        python = { 'pylint' },
       }
+
       --Run linter on save
       vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
-        callback = function() lint.try_lint() end,
+        callback = function()
+          lint.try_lint()
+          lint.try_lint 'cspell'
+          lint.try_lint 'write_good'
+        end,
       })
     end,
   },
